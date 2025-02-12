dc237de885ef6865504ec7afc265b0c3701e1317
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-12 22:18:13 +0000

    feat: add a basic calculator

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,105 @@
+use chumsky::prelude::*;
 use logos::Logos;
 
-#[derive(Logos)]
-#[logos()]
-enum Token {}
+#[derive(Logos, Clone, PartialEq, Hash, Debug, Eq)]
+#[logos(error = String)]
+#[logos(skip r"[ \t\r\n]+")]
+enum Token {
+    #[token("+")]
+    Plus,
+    #[token("-")]
+    Minus,
+    #[token("*")]
+    Multiply,
+    #[token("/")]
+    Divide,
+    #[token("(")]
+    LeftParen,
+    #[token(")")]
+    RightParen,
+    #[regex("[0-9]+", |lex| lex.slice().parse::<isize>().unwrap())]
+    Int(isize),
+}
+
+#[derive(Debug)]
+enum Expr {
+    // Integer literal
+    Int(isize),
+
+    // Unary minus
+    Neg(Box<Expr>),
+
+    // Binary operators
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+}
+
+fn parser() -> impl Parser<Token, Expr, Error = Simple<Token>> {
+    recursive(|p| {
+        let atom = {
+            let parenthesized = p
+                .clone()
+                .delimited_by(just(Token::LeftParen), just(Token::RightParen));
+
+            let integer = select!(|_span|Token::Int(n) => (Expr::Int(n)));
+
+            parenthesized.or(integer)
+        };
+
+        let unary = just(Token::Minus)
+            .repeated()
+            .then(atom)
+            .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
+
+        let binary_1 = unary
+            .clone()
+            .then(
+                just(Token::Multiply)
+                    .or(just(Token::Divide))
+                    .then(unary)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| match op {
+                Token::Multiply => Expr::Mul(Box::new(lhs), Box::new(rhs)),
+                Token::Divide => Expr::Div(Box::new(lhs), Box::new(rhs)),
+                _ => unreachable!(),
+            });
+
+        binary_1
+            .clone()
+            .then(
+                just(Token::Plus)
+                    .or(just(Token::Minus))
+                    .then(binary_1)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| match op {
+                Token::Plus => Expr::Add(Box::new(lhs), Box::new(rhs)),
+                Token::Minus => Expr::Sub(Box::new(lhs), Box::new(rhs)),
+                _ => unreachable!(),
+            })
+    })
+    .then_ignore(end())
+}
 
 fn main() {
-    println!("Hello, world!");
+    let text = "1 + 3 * (12 - 4)";
+
+    let lexer = Token::lexer(text);
+
+    let mut tokens = vec![];
+
+    for (token, span) in lexer.spanned() {
+        match token {
+            Ok(token) => tokens.push(token),
+            Err(e) => {
+                eprintln!("lexer error at {:?}: {}", span, e);
+                return;
+            }
+        }
+    }
+
+    println!("{:#?}", parser().parse(tokens));
 }
