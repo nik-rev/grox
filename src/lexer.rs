54eec90c7bd7c370faa09c7a52e1e471de110817
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-12 23:48:45 +0000

    test: simplify lexer test syntax

--- src/lexer.rs
+++ src/lexer.rs
@@ -44,102 +44,89 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[track_caller]
-    fn lex(input: &str, expected: &[(Result<Token, String>, Range<usize>)]) {
-        let a: Vec<(Result<Token, String>, Range<usize>)> = T::lexer(input).spanned().collect();
-        let b = expected.to_vec();
+    fn lex_valid(source: &str, expected: &[Token]) {
+        let actual: Vec<Token> = T::lexer(source).map(|a| a.unwrap()).collect();
+        let expected = expected.to_vec();
 
-        assert_eq!(b, a);
+        assert_eq!(expected, actual);
     }
 
     #[test]
     fn addition() {
-        lex(
-            "1 + 3",
-            &[
-                (Ok(T::Int(1)), 0..1),
-                (Ok(T::Plus), 2..3),
-                (Ok(T::Int(3)), 4..5),
-            ],
-        );
+        lex_valid("1 + 3", &[T::Int(1), T::Plus, T::Int(3)]);
     }
 
     #[test]
     fn comments() {
-        lex(
-            "1 + 3 // hello world",
-            &[
-                (Ok(T::Int(1)), 0..1),
-                (Ok(T::Plus), 2..3),
-                (Ok(T::Int(3)), 4..5),
-            ],
-        );
-        lex(
+        lex_valid("1 + 3 // hello world", &[T::Int(1), T::Plus, T::Int(3)]);
+        lex_valid(
             "1 + // hello world
             3",
-            &[
-                (Ok(T::Int(1)), 0..1),
-                (Ok(T::Plus), 2..3),
-                (Ok(T::Int(3)), 31..32),
-            ],
+            &[T::Int(1), T::Plus, T::Int(3)],
         );
     }
 
     #[test]
     fn identifier() {
-        lex(
+        lex_valid(
             "1 + hello * 4",
             &[
-                (Ok(T::Int(1)), 0..1),
-                (Ok(T::Plus), 2..3),
-                (Ok(T::Ident("hello")), 4..9),
-                (Ok(T::Multiply), 10..11),
-                (Ok(T::Int(4)), 12..13),
+                T::Int(1),
+                T::Plus,
+                T::Ident("hello"),
+                T::Multiply,
+                T::Int(4),
             ],
         )
     }
 
     #[test]
     fn function() {
-        lex(
+        lex_valid(
             "fn main() {}",
             &[
-                (Ok(T::Ident("fn")), 0..2),
-                (Ok(T::Ident("main")), 3..7),
-                (Ok(T::OpenParen), 7..8),
-                (Ok(T::CloseParen), 8..9),
-                (Ok(T::OpenCurly), 10..11),
-                (Ok(T::CloseCurly), 11..12),
+                T::Ident("fn"),
+                T::Ident("main"),
+                T::OpenParen,
+                T::CloseParen,
+                T::OpenCurly,
+                T::CloseCurly,
             ],
         );
-        lex(
+        lex_valid(
             "\
-fn main() {
+fn main(z, m) {
     let a = 4;
     let x = a * 4;
-    x - a
+    x - z * m
 }",
             &[
-                (Ok(T::Ident("fn")), 0..2),
-                (Ok(T::Ident("main")), 3..7),
-                (Ok(T::OpenParen), 7..8),
-                (Ok(T::CloseParen), 8..9),
-                (Ok(T::OpenCurly), 10..11),
-                (Ok(T::Ident("let")), 16..19),
-                (Ok(T::Ident("a")), 20..21),
-                (Ok(T::Equals), 22..23),
-                (Ok(T::Int(4)), 24..25),
-                (Ok(T::Semicolon), 25..26),
-                (Ok(T::Ident("let")), 31..34),
-                (Ok(T::Ident("x")), 35..36),
-                (Ok(T::Equals), 37..38),
-                (Ok(T::Ident("a")), 39..40),
-                (Ok(T::Multiply), 41..42),
-                (Ok(T::Int(4)), 43..44),
-                (Ok(T::Semicolon), 44..45),
-                (Ok(T::Ident("x")), 50..51),
-                (Ok(T::Minus), 52..53),
-                (Ok(T::Ident("a")), 54..55),
-                (Ok(T::CloseCurly), 56..57),
+                T::Ident("fn"),
+                T::Ident("main"),
+                T::OpenParen,
+                T::Ident("z"),
+                T::Comma,
+                T::Ident("m"),
+                T::CloseParen,
+                T::OpenCurly,
+                T::Ident("let"),
+                T::Ident("a"),
+                T::Equals,
+                T::Int(4),
+                T::Semicolon,
+                T::Ident("let"),
+                T::Ident("x"),
+                T::Equals,
+                T::Ident("a"),
+                T::Multiply,
+                T::Int(4),
+                T::Semicolon,
+                T::Ident("x"),
+                T::Minus,
+                T::Ident("z"),
+                T::Multiply,
+                T::Ident("m"),
+                T::CloseCurly,
             ],
         );
     }
