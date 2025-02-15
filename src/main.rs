7af3971c4f6a69cb4c2498e1547fc0d452c7b149
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-15 14:03:39 +0000

    feat: function codegen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, iter};
 
 use chumsky::Parser as _;
 use inkwell::{
@@ -6,6 +6,7 @@ use inkwell::{
     context::Context,
     execution_engine::ExecutionEngine,
     module::Module,
+    types::BasicMetadataTypeEnum,
     values::{BasicMetadataValueEnum, BasicValueEnum, FloatValue, FunctionValue, PointerValue},
 };
 use logos::Logos as _;
@@ -29,10 +30,100 @@ impl<'ctx> Compiler<'ctx> {
         self.module.get_function(name)
     }
 
+    fn fn_value(&self) -> FunctionValue<'ctx> {
+        self.fn_value_opt.unwrap()
+    }
+
     fn build_load(&self, ptr: PointerValue<'ctx>, name: &str) -> BasicValueEnum<'ctx> {
         self.builder
             .build_load(self.context.f64_type(), ptr, name)
             .unwrap()
+    }
+
+    fn create_entry_block_alloca(&self, name: &str) -> PointerValue<'ctx> {
+        let builder = self.context.create_builder();
+
+        let entry = self.fn_value().get_first_basic_block().unwrap();
+
+        match entry.get_first_instruction() {
+            Some(first_instr) => builder.position_before(&first_instr),
+            None => builder.position_at_end(entry),
+        }
+
+        builder.build_alloca(self.context.f64_type(), name).unwrap()
+    }
+
+    fn compile_fn_decl(&mut self, func: Stmt) -> Result<FunctionValue<'ctx>, &'static str> {
+        let Stmt::FunctionDeclaration { name, params, body } = func else {
+            panic!("Not a function")
+        };
+
+        let return_type = self.context.f64_type();
+        let args_types: Vec<BasicMetadataTypeEnum> = iter::repeat(return_type)
+            .take(params.len())
+            .map(|f| f.into())
+            .collect();
+
+        let fn_type = self.context.f64_type().fn_type(&args_types, false);
+        let fn_val = self.module.add_function(&name, fn_type, None);
+
+        // set argument names
+        for (param, param_name) in fn_val.get_param_iter().zip(params.iter()) {
+            param.into_float_value().set_name(param_name);
+        }
+
+        let entry = self.context.append_basic_block(fn_val, "entry");
+
+        self.builder.position_at_end(entry);
+
+        let len = params.len();
+
+        self.variables.reserve(len);
+
+        for (param, param_name) in fn_val.get_param_iter().zip(params) {
+            let alloca = self.create_entry_block_alloca(&param_name);
+
+            self.builder.build_store(alloca, param).unwrap();
+
+            self.variables.insert(param_name, alloca);
+        }
+
+        let Some(body) = self.compile_stmts(body) else {
+            // TODO: handle functions with no return
+            unreachable!()
+        };
+
+        self.builder.build_return(Some(&body?)).unwrap();
+
+        if fn_val.verify(true) {
+            Ok(fn_val)
+        } else {
+            // SAFETY: fn_val is not used after being deleted as it is dropped
+            // and not returned from the function
+            unsafe {
+                fn_val.delete();
+            }
+
+            Err("Invalid generated function")
+        }
+    }
+
+    /// Returns Some(Result<FloatValue>) if the statement is a return. Stops
+    /// processing all statements in that case.
+    fn compile_stmts(
+        &mut self,
+        stmts: Vec<Stmt>,
+    ) -> Option<Result<FloatValue<'ctx>, &'static str>> {
+        for stmt in stmts {
+            match stmt {
+                Stmt::FunctionDeclaration { name, params, body } => todo!(),
+                Stmt::VariableDeclaration { name, value } => todo!(),
+                Stmt::VariableAssignment { name, value } => todo!(),
+                Stmt::Return(expr) => return Some(self.compile_expr(*expr)),
+            }
+        }
+
+        None
     }
 
     fn compile_expr(&mut self, expr: Expr) -> Result<FloatValue<'ctx>, &'static str> {
